9f819c76cdb4a60e60505d06179167f0ee7b993c
JoshWacks <2143116@students.wits.ac.za>
2020-06-29 10:25:49 +0200

    Final Version

--- app/src/main/java/com/example/petrolapp/AddCards.java
+++ app/src/main/java/com/example/petrolapp/AddCards.java
@@ -4,7 +4,9 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import org.json.JSONArray;
@@ -15,18 +17,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class AddCards extends AppCompatActivity {
-
-    private static String username;
+    static String username;
     String company,card_number,exp_date,cardid;
     String allTheCardNos;
     ArrayList<String> Card_Nos;
 
+    private Spinner spinner1,spinner2,spinner3;
+    private static final String[] allottedCompanys = {"edgars", "woolworths", "shell","discovery","dis_chem","clicks"};
+    private static final String[] allottedMonths = {"01", "02", "03","04","05","06","07","08","09","10","11","12"};
+    private static final String[] allottedYears = {"2021", "2022", "2023","2024","2025","2026","2027","2028","2029","2030"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cards);
-        username=appInformation.getUsername() ;
+
+        username=appInformation.getUsername();
 
         ContentValues cv=new ContentValues();
         cv.put("USERNAME",username);
@@ -41,6 +47,28 @@ public class AddCards extends AppCompatActivity {
                 }
             }
         });
+
+
+        //determining Comapny
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(AddCards.this,
+                android.R.layout.simple_spinner_item, allottedCompanys);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        //determining month
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(AddCards.this,
+                android.R.layout.simple_spinner_item, allottedMonths);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        //determining year
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(AddCards.this,
+                android.R.layout.simple_spinner_item, allottedYears);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
     }
 
     private void processJSON (String json) throws JSONException {
@@ -65,18 +93,20 @@ public class AddCards extends AppCompatActivity {
     public void doAdd(View view) {
 
         GetData();
-        InsertData(company,card_number,exp_date);
     }
     private void GetData() {
-
-        EditText Comapny = (EditText) findViewById(R.id.editCompany);
+        String month,year;
         EditText Card_number = (EditText) findViewById(R.id.editCard_Number);
-        EditText Expiry_date = (EditText) findViewById(R.id.editExpiry_Date);
-
-        company = Comapny.getText().toString();
-        company.toLowerCase();
         card_number = Card_number.getText().toString();
-        exp_date = Expiry_date.getText().toString();
+
+        company= (String) spinner1.getSelectedItem();
+        month= (String) spinner2.getSelectedItem();
+        year= (String) spinner3.getSelectedItem();
+        exp_date = year+"-"+month+"-01";
+
+
+        InsertData(company,card_number,exp_date);
+
     }
 
     public void InsertData(final String company, final String card_number, final String exp_date){
@@ -88,29 +118,48 @@ public class AddCards extends AppCompatActivity {
         params.put("EXPIRY_DATE", exp_date);
 
 
-        if(checkCardNo(card_number)==0 ){
+        if(!checkCardNo(card_number) &&!isempty(company,card_number,exp_date) && validateCN(card_number)){
+
             Connection c=new Connection("https://lamp.ms.wits.ac.za/home/s2143116/");
             c.fetchInfo(AddCards.this, "NewCard",params, new RequestHandler() {
                 @Override
                 public void processResponse(String response) {
-                    Toast.makeText(AddCards.this, "Data Submit Successfully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(AddCards.this, "Data Submitted Successfully", Toast.LENGTH_LONG).show();
 
                 }
             });
         }
-        else{
-            Toast.makeText(AddCards.this, "Card Number already exists", Toast.LENGTH_LONG).show();
 
-        }
+
 
     }
-    private int checkCardNo(String input) {
+    private boolean isempty(String company, String card_number, String exp_date) {
+        if(card_number.isEmpty()) {
+            Toast.makeText(AddCards.this, "card number is empty", Toast.LENGTH_LONG).show();
+            Toast.makeText(AddCards.this, "please enter valid inputs", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkCardNo(String input) {
         if(Card_Nos.contains(input)){
-            return 1;
+            Toast.makeText(AddCards.this, "card number already exists", Toast.LENGTH_LONG).show();
+            return true;
         } else{
-            return 0;
+            return false;
         }
     }
+
+    public boolean validateCN(String input){
+        String [] spl=input.split(" ");
+        if(spl.length==4 && (spl[0].matches("^[0-9]*$") && spl[0].length() == 4) && (spl[1].matches("^[0-9]*$") && spl[1].length() == 4) &&(spl[2].matches("^[0-9]*$") && spl[2].length() == 4) && (spl[3].matches("^[0-9]*$") && spl[3].length() == 4)){
+            return true;
+        }
+        Toast.makeText(AddCards.this, "Enter Valid Card number", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
     public void bttnBack(View view) {
         Intent myIntent = new Intent(AddCards.this, MainMenuActivity.class);        //go back to main menu
 
--- app/src/main/java/com/example/petrolapp/AddCars.java
+++ app/src/main/java/com/example/petrolapp/AddCars.java
@@ -4,8 +4,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,19 +12,33 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class AddCars extends AppCompatActivity {
+    static String username;
+    String lisc_plate,brand,model,year;
+    String allTheLiscPlates;
+    ArrayList<String> Lisc_list;
 
-    private static final String username=appInformation.getUsername();
+    private Spinner spinner1,spinner2,spinner3;
+    private static final String[] allottedBrands = {"Nissan", "Toyota", "VolksWagen","Ford","BMW","Mercedes","Audi"};
+    private static final String[] allottedModelsNissan={"Coupe 370Z","GT-R","Navara","NP300 HardBody","Micra","Micra Active","Qashqai","NV 350","Almera","NP200","NP200 HardBody","Patrol","XTrail"};
+    private static final String[] allottedModelsToyota={"86","Avanza" ,"Aygo","Corolla","Corolla Hatch","Corolla Quest","Etios HAtch","Etios Sedan","Prius","Supra","Yaris","C_HR","FJ Cruiser","Fortuner","Land Cruiser 200","Land Cruiser Prado","Rav4","Rush","Avanza Panel Van","Hiace","Hiace Ses'fikile","Hilux Double Cab","Hilux single Cab","Hilux Extra Cab","Land Cruiser 76","Land Cruiser 79","Quantum Bus","Quatum Panel Van","Aygo",};
+    private static final String[] allottedModelsVolkswagen={"Amarok","Golf","Polo","Polo Vivo","Up!","T Cross","Caddy","California","Caravelle","Kombi","Transporter","Tiguan","Tiguan AllSpace","Touareg"};
+    private static final String[] allottedModelsFord={"Figo","Fiesta","Mustang","EcoSport","Everest","Kuga","Ranger","Trasit Custom","Transit single Chassis Cab","Transit Van","Tourneo Custom","Raptor"};
+    private static final String[] allottedModelsBMW={"220i","M240i","M2","218i","220d","M235i","318i ","320i","330I","320d","330d","420i","440i","420d","m4","540i","520d","530d"};
+    private static final String[] allottedModelsMercedes={"A Class","B Class","C Class","CLA","CLS","E Class","G Class","GLA","GLB","GLC","GLE","GLS","S Class","SL","V Class","Vito","X Class"};
+    private static final String[] allottedModelsAudi={"A1","S1","A3","S3","RS3","A4","S4","RS4","A5","S5","RS5","A6","S6","RS6","A7","S7","RS7","Q2","Q3","RS Q3","Q5","SQ5","Q7","Q8","TT",};
+    private static final String[] allottedYears = {"2010", "2011", "2012","2013","2014","2015","2016","2017","2018","2019","2020",};
 
-    private String lisc_plate,brand,model,year;
-    private String allTheLiscPlates;
-    private ArrayList<String> Lisc_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+       username=appInformation.getUsername();
 
         ContentValues cv=new ContentValues();
         cv.put("USERNAME",username);
@@ -41,6 +54,39 @@ public class AddCars extends AppCompatActivity {
             }
         });
 
+
+        //determining year
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(AddCars.this,
+                android.R.layout.simple_spinner_item, allottedYears);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+
+        //determining brand
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(AddCars.this,
+                android.R.layout.simple_spinner_item, allottedBrands);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedBrand= (String) spinner1.getSelectedItem();
+                switch (selectedBrand){
+                    case "Nissan":        creatingDropBox(allottedModelsNissan); break;
+                    case "Toyota":        creatingDropBox(allottedModelsToyota); break;
+                    case "Volkswagen":        creatingDropBox(allottedModelsVolkswagen); break;
+                    case "Ford":        creatingDropBox(allottedModelsFord); break;
+                    case "BMW":        creatingDropBox(allottedModelsBMW); break;
+                    case "Mercedes":        creatingDropBox(allottedModelsMercedes); break;
+                    case "Audi":        creatingDropBox(allottedModelsAudi); break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 
@@ -62,25 +108,23 @@ public class AddCars extends AppCompatActivity {
         Lisc_list = new ArrayList<>(Arrays.asList(allTheLiscPlates.split(",")));
     }
 
-    public void doAdd(View view) {
+    public void creatingDropBox(String [] input) {
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(AddCars.this,
+                android.R.layout.simple_spinner_item, input);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
 
-        GetData();
+    }
+    public void doAdd(View view) {
+        EditText editLisc = (EditText) findViewById(R.id.editLiscPlate);
+        lisc_plate = editLisc.getText().toString().toUpperCase();
+        brand= (String) spinner1.getSelectedItem();
+        model= (String) spinner2.getSelectedItem();
+        year= (String) spinner3.getSelectedItem();
+
         InsertData(lisc_plate, brand, model, year);
     }
-
-    private void GetData() {
-
-        EditText editLisc = (EditText) findViewById(R.id.editLiscPlate);
-        EditText editBrand = (EditText) findViewById(R.id.editBrand);
-        EditText editModel = (EditText) findViewById(R.id.editModel);
-        EditText editYear = (EditText) findViewById(R.id.editYear);
-
-        lisc_plate = editLisc.getText().toString();
-        brand = editBrand.getText().toString();
-        model = editModel.getText().toString();
-        year = editYear.getText().toString();
-    }
-
 
     public void InsertData(final String lisc_plate, final String brand, final String model, final String year){
 
@@ -91,40 +135,77 @@ public class AddCars extends AppCompatActivity {
         params.put("CAR_MODEL", model);
         params.put("CAR_YEAR", year);
 
-        if(checkLisc(lisc_plate)==0 ){
-            System.out.println("this is complete bull");
+        if(!checkLisc(lisc_plate) && !isempty(lisc_plate,model,brand,year) && validateL(lisc_plate) ){
             Connection c=new Connection("https://lamp.ms.wits.ac.za/home/s2143116/");
             c.fetchInfo(AddCars.this, "NewCar",params, new RequestHandler() {
                 @Override
                 public void processResponse(String response) {
-                    Toast.makeText(AddCars.this, "Data Submit Successfully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(AddCars.this, "Data Submitted Successfully", Toast.LENGTH_LONG).show();
 
                 }
             });
         }
-        else{
-            System.out.println("this is huge bull");
-            Toast.makeText(AddCars.this, "Liscence plate already exists", Toast.LENGTH_LONG).show();
-
-        }
-
     }
 
-    private int checkLisc(String input) {
-        System.out.println("this is bull");
+    private boolean isempty(String lisc_plate, String model, String brand, String year) {
+        if(lisc_plate.isEmpty()){
+            Toast.makeText(AddCars.this, "Liscence plate is empty", Toast.LENGTH_LONG).show();
+            Toast.makeText(AddCars.this, "please enter valid inputs", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkLisc(String input) {
         if(Lisc_list.contains(input)){
-            System.out.println("this is absolute bull");
-            //Toast.makeText(AddCars.this, "Liscence plate already exists", Toast.LENGTH_LONG).show();
-            return 1;
-        } else{
-            return 0;
-        }
+            Toast.makeText(AddCars.this, "Liscence plate already exists", Toast.LENGTH_LONG).show();
+            return true;
+        } return false;
     }
+
+    public boolean validateL(String input){
+        String [] spl=input.split(" ");   //spl=split liscence plate
+        boolean temp=false;
+        switch(spl[spl.length-1]){
+            case "WP": temp=checkTypes(spl,"WP");  break;
+            case "ZN": temp=checkTypes(spl,"ZN");  break;
+            case "MP": temp=checkTypes(spl,"MP");  break;
+            case "EC": temp=checkTypes(spl,"EC");  break;
+            case "L":  temp=checkTypes(spl,"L");  break;
+            case "GP": temp=checkTypes(spl,"GP");  break;
+            case "FS": temp=checkTypes(spl,"FS");  break;
+            case "NW": temp=checkTypes(spl,"NW");  break;
+
+        }
+        Toast.makeText(AddCars.this, "Enter Valid Liscence Plate number", Toast.LENGTH_LONG).show();
+        return temp;
+
+
+    }
+    public boolean checkTypes(String[] spl,String input){
+
+        if(Objects.equals(spl[spl.length-1], input)) {
+            System.out.println(input);
+
+            if ( ( (Pattern.matches("[a-zA-Z]+", spl[0]) && spl[0].length() == 3) && (spl[1].matches("^[0-9]*$") && spl[1].length() == 3) ) || (Pattern.matches("[a-zA-Z]+", spl[0]) && spl[0].length() < 7)) {
+
+                return true;            // validating for types xxx 000 GP AND custom
+            } else if ((Pattern.matches("[a-zA-Z]+", spl[0]) && spl[0].length() == 2) && (spl[1].matches("^[0-9]*$") && spl[1].length() == 2) && (Pattern.matches("[a-zA-Z]+", spl[2]) && spl[2].length() == 2)) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+
 
 
     public void bttnBack(View view) {
         Intent myIntent = new Intent(AddCars.this, MainMenuActivity.class);        //go back to main menu
-
         startActivity(myIntent);
     }
 }
